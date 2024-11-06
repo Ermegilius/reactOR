981ecc1a50df32f01063ad6753dafd8e263e7efa
Beliakov Vladimir <ermegilius@gmail.com>
2024-11-06 19:24:53 +0200

    update: EmployeeCard.jsx. Finnish with the functionality

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,16 +1,33 @@
 import './EmployeeCard.css'
 import {useState} from 'react'
 
+const currentDate = new Date();
+
 function EmployeeCard(props) {
   const [teamLead, setTeamLead] = useState(props.teamLead);
+  const currentDate = new Date();
+  const startDate = new Date(props.startDate);
 
   const clickHandler = () => {
     setTeamLead(!teamLead);
   }
+  const yearsWorked = currentDate.getFullYear() - startDate.getFullYear();
+
+  const monthsWorked = () => {
+    let yearsDifference = currentDate.getFullYear() - startDate.getFullYear();
+    let monthsDifference = currentDate.getMonth() - startDate.getMonth();
+    if (monthsDifference < 0) {
+        yearsDifference -= 1;
+        monthsDifference += 12;
+    }
+    const monthsWorked = yearsDifference * 12 + monthsDifference;
+    return monthsWorked;
+  };
+
+  const congrats = "Schedule recognition meeting."
   
   return (
     <div className="card">
-
       <p>Name: {props.name}</p>
       <p>Role:{(teamLead === true)?
         (<span>⭐</span>):('')
@@ -19,7 +36,13 @@ function EmployeeCard(props) {
       <p>Salary: {props.salary}</p>
       <p>Birth: {props.birth}</p>
       <p>Location: {props.location}</p>
-      <p></p>
+      <p>Years in the company: {yearsWorked}</p>
+      {(yearsWorked%5===0&&yearsWorked!==0)?
+      (<p>&#127183;{congrats}</p>):('')
+      }
+      {(monthsWorked()<6)?
+      (<p>&#9997;Schedule probation review.</p>):('')
+      }
       <button onClick={clickHandler}>
         {(teamLead === false) ?
         ('Promote to Team Lead'):('Demote from Team Lead')}
