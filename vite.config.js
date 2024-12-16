3112a972ca39d5795eb776a024d475e06dc73224
Vladimir Beliakov <ermegilius@gmail.com>
2024-12-16 18:27:17 +0200

    Fix paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/reactor/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          title: 'HR app',
+        },
+      },
+    }),
+  ],
 })
