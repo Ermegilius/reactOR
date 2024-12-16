c85c59f86db63dcbfd0924c82b27efba722da21e
Beliakov Vladimir <ermegilius@gmail.com>
2024-12-16 13:47:59 +0200

    refactor: server.js to ES6

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+import jsonServer from 'json-server';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "src", "data", "db.json"));
+const router = jsonServer.router(path.join(__dirname, 'src', 'data', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
