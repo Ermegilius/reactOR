a604c2749aec8d559f32c78725863e030b2081d9
Beliakov Vladimir <ermegilius@gmail.com>
2024-12-18 13:26:47 +0200

    add: login logic. Users are inside db.json for now

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,72 @@
-import Button from "../components/Button/Button"
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button/Button';
+import styles from './Login.module.css';
 
-const Login = ({ LogInAction , loggedIn}) => {//login logic removed until next times
+const Login = ({ LogInAction, loggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [users, setUsers] = useState([]);
   const navigate = useNavigate();
-  const loginHandler = () => {
-    LogInAction();
-    navigate('/');
-  }
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/users');
+        if (!response.ok) {
+          throw new Error("Network error. Can't get users db");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError('Failed to fetch users');
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    const user = users.find(user => user.username === username && user.password === password);
+    if (user) {
+      LogInAction(user.userId, username, password);
+      navigate('/');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
 
   return (
-    <>
-      <Button onClick={loginHandler} text= {loggedIn ? "Log out" : "Log in" }/>
-    </>
+    <div className={styles.container}>
+      <h2>Login</h2>
+      <form onSubmit={loginHandler}>
+        <div className={styles['form-group']}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles['form-group']}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p className={styles.error}>{error}</p>}
+        <Button type="submit" text="Log in" className={styles.loginButton} />
+      </form>
+    </div>
   );
 };
-  
+
 export default Login;
