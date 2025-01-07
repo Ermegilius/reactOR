964f46c7d60b04c6b6b3df6625aa8e7d4df78d2f
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-07 13:01:27 +0200

    fix: minor issues

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const cors = require('cors');
-const path = require('path');
-const jsonServer = require('json-server');
+import { backEndUrl } from "./src/data/globalVariables.js";
+
+const cors = require("cors");
+const path = require("path");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'src', 'data', 'db.json'));
+const router = jsonServer.router(
+  path.join(__dirname, "src", "data", "db.json")
+);
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
@@ -10,8 +14,9 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`JSON Server is running on ${backEndUrl}:${PORT}`);
+  //console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,72 +1,89 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import EmployeeCard from '../EmployeeCard/EmployeeCard.jsx';
-import styles from './EmployeeList.module.css';
-import useAxios from '../../utilis/useAxios.js';
-import { backEndUrl } from '../../data/globalVariables.js';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import EmployeeCard from "../EmployeeCard/EmployeeCard.jsx";
+import styles from "./EmployeeList.module.css";
+import useAxios from "../../utilis/useAxios.js";
+import { backEndUrl } from "../../data/globalVariables.js";
 
 function EmployeeList() {
-    const { data: persons = [], loading, get, update, remove } = useAxios(`${backEndUrl}/persons`); // use custom hook to get data from the server
-    const navigate = useNavigate();
-    const [employees, setEmployees] = useState([]); // Define the state for employees
+  const {
+    data: persons = [],
+    loading,
+    get,
+    update,
+    remove,
+  } = useAxios(`${backEndUrl}/persons`); // use custom hook to get data from the server
+  const navigate = useNavigate();
+  const [employees, setEmployees] = useState([]); // Define the state for employees
 
-    useEffect(() => {
-        const getPersons = async () => {
-            await get('');
-        };
-        getPersons();
-    }, []);
-
-    useEffect(() => {
-        if (Array.isArray(persons)) {
-            setEmployees(persons);
-        } else {
-            setEmployees([]);
-        }
-    }, [persons]);
-
-    const handleNavigate = (id) => {
-        navigate(`/employees/${id}`);
+  useEffect(() => {
+    const getPersons = async () => {
+      await get("");
     };
+    getPersons();
+  }, []);
 
-    //function to update the person data
-    const updatePerson = async (updatedPerson) => {
-        try {
-            await update(`/${updatedPerson.id}`, updatedPerson);
-            setEmployees((prevPersons) =>
-                Array.isArray(prevPersons) ? prevPersons.map((person) =>
-                    person.id === updatedPerson.id ? updatedPerson : person
-                ) : []
-            );
-        } catch (error) {
-            console.error("Failed to update person data", error);
-        }
-    };
+  useEffect(() => {
+    if (Array.isArray(persons)) {
+      setEmployees(persons);
+    } else {
+      setEmployees([]);
+    }
+  }, [persons]);
 
-    //function to delete the person
-    const deletePerson = async (id) => {
-        try {
-            await remove(`/${id}`);
-            setEmployees((prevPersons) =>
-                Array.isArray(prevPersons) ? prevPersons.filter((person) => person.id !== id) : []
-            );
-        } catch (error) {
-            console.error("Failed to delete person", error);
-        }
-    };
+  const handleNavigate = (id) => {
+    navigate(`/employees/${id}`);
+  };
 
-    return (
-        <div className={styles.list}>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                Array.isArray(employees) && employees.map((person) => (
-                    // Spreading all properties of the employee object as props for EmployeeCard
-                    <EmployeeCard key={person.id} {...person} handleNavigate={handleNavigate} updatePerson={updatePerson} deletePerson={deletePerson}/>
-                ))
-            )}
-        </div>
-    );
+  //function to update the person data
+  const updatePerson = async (updatedPerson) => {
+    try {
+      await update(`/${updatedPerson.id}`, updatedPerson);
+      setEmployees((prevPersons) =>
+        Array.isArray(prevPersons)
+          ? prevPersons.map((person) =>
+              person.id === updatedPerson.id ? updatedPerson : person
+            )
+          : []
+      );
+    } catch (error) {
+      console.error("Failed to update person data", error);
+    }
+  };
+
+  //function to delete the person
+  const deletePerson = async (id) => {
+    try {
+      await remove(`/${id}`);
+      setEmployees((prevPersons) =>
+        Array.isArray(prevPersons)
+          ? prevPersons.filter((person) => person.id !== id)
+          : []
+      );
+    } catch (error) {
+      console.error("Failed to delete person", error);
+    }
+  };
+
+  return (
+    <div className={styles.list}>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        Array.isArray(employees) &&
+        employees.map((person) => (
+          // Spreading all properties of the employee object as props for EmployeeCard
+          <EmployeeCard
+            key={person.id}
+            {...person}
+            handleNavigate={handleNavigate}
+            updatePerson={updatePerson}
+            deletePerson={deletePerson}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default EmployeeList;
