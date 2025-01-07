4d912ca29c838f47b4451834ebf36637b243c734
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-07 13:15:02 +0200

    fix: minor issues

--- src/data/globalVariables.js
+++ src/data/globalVariables.js
@@ -1,9 +1,9 @@
-'use strict';
-
-//for my deployed on Render server:
-const backEndUrl = 'https://reactor-uinv.onrender.com';
+"use strict";
 
 //for local server:
 //const backEndUrl = 'http://localhost:3001';
+
+//for my deployed on Render server:
+const backEndUrl = "https://reactor-uinv.onrender.com";
 
 export { backEndUrl };
